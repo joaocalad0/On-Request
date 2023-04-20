fd993f2df92f3932d5f0e4c2e797d46c88225a0c
joaocalad0 <joaocalado33@gmail.com>
2023-04-20 02:25:56 +0100

    Layout activitys 1 and 2

--- app/src/main/java/com/example/onrequest/MainActivity.java
+++ app/src/main/java/com/example/onrequest/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.onrequest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void imageView1 (View view) {
+        Intent imageView1 = new Intent(this, MainActivity2.class);
+        startActivity(imageView1);
     }
 }
