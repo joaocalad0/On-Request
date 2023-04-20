7546ed0c5287f4e7575d528d153a70df2c74d1e1
joaocalad0 <joaocalado33@gmail.com>
2023-04-20 11:34:32 +0100

    Layout Activity 3

--- app/src/main/java/com/example/onrequest/MainActivity2.java
+++ app/src/main/java/com/example/onrequest/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.onrequest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void submit (View view) {
+        Intent submit = new Intent(this, MainActivity3.class);
+        startActivity(submit);
     }
 }
