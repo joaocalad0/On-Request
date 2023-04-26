9cc532274cc81c15c15495400ded442ecfc2d23e
joaocalad0 <joaocalado33@gmail.com>
2023-04-26 19:45:11 +0100

    Adding OnClick to MenuList

--- app/src/main/java/com/example/onrequest/MainActivity4.java
+++ app/src/main/java/com/example/onrequest/MainActivity4.java
@@ -6,28 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity4 extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        // Definir uma lista de Menu (em memória) para ser apresentada na RecyclerView
-        List<Menu> menuList = new ArrayList<>();
-        menuList.add(new Menu("Carlos Sancho", "12345678"));
-        menuList.add(new Menu("ABC", "12345678"));
-        menuList.add(new Menu("aaaa", "hdhdhdh"));
-        menuList.add(new Menu("XYZ", "12345678"));
-        menuList.add(new Menu("Menu 1", "12345678"));
-
         // obter uma referência para a RecyclerView que existe no layout da MainActivity4
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
         // criar um objeto do tipo MenuAdapter (que extende Adapter)
-        MenuAdapter adapter = new MenuAdapter(menuList);
+        MenuAdapter adapter = new MenuAdapter(MemoryDataBase.getAllMenus());
         // criar um objecto do tipo LinearLayoutManager para ser utilizado na RecyclerView
         // o LinearLayoutManager tem como orientação default a orientação Vertical
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
